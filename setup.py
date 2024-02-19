0a7e766855934e25dadc7540b5347862cc6296ec
phanideva <phanisaisri@gmail.com>
2024-02-19 10:34:04 -0600

    requirements & installation done

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "phanideva"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "phanisaisri@gmail.com"
 
 
 
